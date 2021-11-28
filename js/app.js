fbdbf09c8bcc38be756f2ab26eedcc5f44907518
Zima <lontuku@gmail.com>
2021-11-27 22:37:07 -0300

    agregar formulario

--- js/app.js
+++ js/app.js
@@ -20,7 +20,6 @@ $(document).scroll(function() {
   hr_scroll();
 });
 
-hr_scroll();
 
 function hr_scroll() {
   var scroll_top = $window.scrollTop(),
@@ -47,3 +46,70 @@ function hr_scroll() {
     
   });
 }
+
+// handle links with @href started with '#' only
+$(document).on('click', 'a[href^="#"]', function(e) {
+  // target element id
+  var id = $(this).attr('href');
+
+  // target element
+  var $id = $(id);
+  if ($id.length === 0) {
+      return;
+  }
+  // prevent standard hash navigation (avoid blinking in IE)
+  e.preventDefault();
+  // top position relative to the document
+  var pos = $id.offset().top - 80;
+  
+  // animated top scrolling
+  $('body, html').animate({scrollTop: pos}, 50);
+});
+
+
+//formulario
+let good = $('#good');
+let fail = $('#fail');
+
+$(document).on('click', function(event) {
+  if (good.css('display') == 'grid') {
+    modal = good;
+  }
+  else if (fail.css('display') == 'grid') {
+    modal = fail;
+  };
+  if (typeof modal !== 'undefined') {
+    if (!$(event.target).closest(".modal-content, .send").length || event.target.className == 'close-btn') {
+      modal.css('opacity', '0');
+      modal.one('transitionend', function() {
+        modal.css('display', 'none');
+        modal.css('opacity', '1');
+      });
+    };
+  };
+});
+
+$('#theform').submit(function(event) {
+  event.preventDefault();
+// Prevent the form from submitting via the browser 
+  console.log('asd');
+  var form = $(this);
+    $.ajax({
+      type: form.attr('method'),
+      url: form.attr('action'),
+      data: form.serialize()
+    }).done(function(data) {
+      good.css({
+        'opacity': '1',
+        'display': 'grid',
+        'place-items': 'center',
+      });
+    }).fail(function(data) {
+      fail.css({
+        'opacity': '1',
+        'display': 'grid',
+        'place-items': 'center',
+      });
+    });
+});
+
